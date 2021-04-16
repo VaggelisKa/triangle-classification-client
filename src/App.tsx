85f2e7f864ebf68905693a03692781f1f2573d2a
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2021-04-16 21:16:50 +0200

    Adding a spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { FC, lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
+import Spinner from './components/spinner/spinner.component';
 
 const HomePage = lazy(() => import('./pages/home-page/home-page.component'));
 
@@ -11,7 +12,7 @@ const App: FC = () => {
     <div className="App">
       <Header />
       <Switch>
-        <Suspense fallback={<>Loading</>} >
+        <Suspense fallback={<Spinner />}>
           <Route exact path="/" component={HomePage} />
         </Suspense>
       </Switch>
